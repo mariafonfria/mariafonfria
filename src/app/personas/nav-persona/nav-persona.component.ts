a102b665d6e5b4e2a2da6442c71ef47c40d34b7f
Voces Porteiro <mikel.vocesporteiro@atos.net>
2017-12-25 08:14:34 +0100

    componente barra nav personas

---
+++ src/app/personas/nav-persona/nav-persona.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-persona',
+  templateUrl: './nav-persona.component.html',
+  styleUrls: ['./nav-persona.component.css']
+})
+export class NavPersonaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
