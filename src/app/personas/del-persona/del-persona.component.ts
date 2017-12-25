347cca3fefcf2ec3766d6840a0029fc4eb4f38c9
Voces Porteiro <mikel.vocesporteiro@atos.net>
2017-12-25 06:52:29 +0100

    ts de del-persona da warning pero funciona

--- src/app/personas/del-persona/del-persona.component.ts
+++ src/app/personas/del-persona/del-persona.component.ts
@@ -9,6 +9,16 @@ export class DelPersonaComponent implements OnInit {
 
   constructor() { }
 
+  confirmar() {
+    var answer = confirm("¿ Estas seguro de eliminar a este usuario?");
+    
+    if ( answer ) {
+      //lo deberia borrar conquery
+    } else {
+      //voy a administracion segun mockup
+    }
+  }
+
   ngOnInit() {
   }
 
